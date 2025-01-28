253a340d2eb2dd6a218ba0959e36a741ccecf5c7
Madhan <mathanmathan98234@gmail.com>
2025-01-27 17:36:39 -0800

    adding end point

--- src/pages/RegisterationForm.jsx
+++ src/pages/RegisterationForm.jsx
@@ -2,35 +2,49 @@ import "./Registeration.css"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faEnvelope,faLock } from '@fortawesome/free-solid-svg-icons'
 import { useState } from "react"
+import { useSelector,useDispatch } from "react-redux"
+import { registerationAsync } from "../redux/slice"
 
 
 const RegisterationForm = () =>{
-    const [action, setAction] = useState("Sign Up")
 
+    const [email, SetEmail] = useState("")
+    const [password, SetPassword] = useState("")
+    const dispatch = useDispatch()
+    const {registerError,registerLoading} = useSelector(state => state.auth)
+
+    const handleSubmit = () =>{
+        let obj = {email: email, password : password}
+        dispatch(registerationAsync(obj))
+
+    }
     return(
         <>
-        <form>
+
         <div className="container">
             <div className="header">
-                <h3>{action}</h3>
+                <h3>Sign Up</h3>
                 <div className="line"></div>
             </div>
             <div className="inputs">
                 <div className="input">
                 <FontAwesomeIcon icon={faEnvelope} className="logo" />
-                    <input type="email" placeholder="Email" />
+                    <input type="email" placeholder="Email" value={email}  onChange={e => SetEmail(e.target.value)}/>
                 </div>
                 <div className="input">
                 <FontAwesomeIcon icon={faLock} className="logo" />
-                    <input type="password" placeholder="Password" />
+                    <input type="password" placeholder="Password"  value={password} onChange={e =>SetPassword(e.target.value)}/>
                 </div>
+               
             </div>
+             <p className="Error">{  registerError && "Failed to fetch"}</p>
             <div className="submit-container">
-                <button className="submit" type="submit">{action}</button>   
+                <button className="submit" type="submit" onClick={handleSubmit} disabled={registerLoading} >{ registerLoading ? "Sign Up...." : "Sign Up"}</button>   
             </div>
-            <div className="login-link"><p>Already have a account ?{" "}<a href="#">Sign in</a></p></div>
+            <div className="login-link"><p>Already have a account ?{" "}<a href="#">Sign In</a></p></div>
         </div>
-        </form>
+        
+
         </>
     )
 }
--- src/redux/slice.js
+++ src/redux/slice.js
@@ -1,7 +1,26 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 
 
+export const registerationAsync = createAsyncThunk("auth/registeration", async(registerationData,{rejectWithValue})=>{
 
+    try{
+        const response = await fetch ("https://todo-learnz-development-hubs-projects.vercel.app/register",{
+            method: "POST",
+            headers:{
+                "Content-Type" : "application/json"
+            }, 
+            body: JSON.stringify(registerationData)
+        })
+
+        if(!response.ok){
+            throw new Error("Error:Registeration failed")
+        }
+        return await response.json()
+
+    } catch (error){
+        return rejectWithValue(error.message) 
+    }
+})
 
 
 
@@ -9,9 +28,30 @@ import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 const authSlice = createSlice({
     name : "auth",
     initialState: {
+        data:{},
         registerError: null,
         registerLoading: false,
-    }, reducers:{
+        registerStatus: 'idle'
+    }, 
+    extraReducers:(builder) =>{
+        builder
+        .addCase(registerationAsync.pending, (state)=>{
+            state.registerLoading = true 
+            state.registerError = null 
+            state.registerStatus = "pending"
+        })
+        .addCase(registerationAsync.fulfilled, (state, action)=>{
+            state.registerLoading = false
+            state.data = action.payload
+            state.registerStatus = "success"
+        })
+        .addCase(registerationAsync.rejected, (state, action)=>{
+            state.registerLoading = false 
+            state.registerError = action.payload 
+            state.registerStatus = "failed"
+        })
         
     }
 })
+
+export default authSlice.reducer;
